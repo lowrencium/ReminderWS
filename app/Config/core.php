a10bb9232eba0ede391e7622603c1c16ade16477
Emile <emile_taverne@hotmail.com>
2013-12-26 11:37:04 +0100

    Modification configuration

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'Krh43o7GJKP0Rt2y3Son');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '314663340503881505367791148274');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
