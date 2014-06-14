40ee64b207b6753925798132e707af567ad3cd84
Emile Taverne <emile_taverne@hotmail.com>
2014-06-14 20:58:01 +0200

    Ajout de la méthode de suppression d'un contact

--- Application/Action/ContactAction.php
+++ Application/Action/ContactAction.php
@@ -7,6 +7,7 @@ class ContactAction implements IAction
         {
             $server->addFunction("AjouterContact");
             $server->addFunction("RecupererContacts");
+            $server->addFunction("SupprimerContact");
         }
         elseif($server instanceof nusoap_server)
         {
@@ -14,6 +15,8 @@ class ContactAction implements IAction
             $server->register("AjouterContact", array("id" => "xsd:string", "token" => "xsd:string", "nom" => "xsd:string", "email" => "xsd:string", "telephone" => "xsd:string", "adresse" => "xsd:string"), array("return" => "tns:AjouterContactIO"));
             RecupererContactsIO::addType($server);
             $server->register("RecupererContacts", array("id" => "xsd:string", "token" => "xsd:string"), array("return" => "tns:RecupererContactsIO"));
+            SupprimerContactIO::addType($server);
+            $server->register("SupprimerContact", array("id" => "xsd:string", "token" => "xsd:string", "email" => "xsd:string"), array("return" => "tns:SupprimerContactIO"));
         }
     }
 }
@@ -152,5 +155,72 @@ function RecupererContacts($id, $token)
     }
 
     $sortie->setResultat(true);
+    return $sortie->toArray();
+}
+
+/**
+ * @param string $id
+ * @param string $token
+ * @param string $email
+ * @return array
+ */
+function SupprimerContact($id, $token, $email)
+{
+    $sortie = new SupprimerContactIO();
+
+    try
+    {
+        $dataAdapter = new AccountData();
+    }
+    catch(Exception $e)
+    {
+        $sortie->setErreur($e);
+        return $sortie->toArray();
+    }
+
+    try
+    {
+        $dataAdapter = new ContactData();
+        $data = $dataAdapter->RecupererContacts($id);
+    }
+    catch(Exception $e)
+    {
+        $sortie->setErreur($e);
+        return $sortie->toArray();
+    }
+
+    $row = $data->fetch(PDO::FETCH_OBJ);
+    if(empty($row->contacts))
+    {
+        $result = array();
+    }
+    else
+    {
+        $result = json_decode($row->contacts, true);
+    }
+
+    if(array_key_exists($email, $result))
+    {
+        unset($result[$email]);
+        try
+        {
+            $data = $dataAdapter->SetContacts($id, json_encode($result));
+        }
+        catch(Exception $e)
+        {
+            $sortie->setErreur($e);
+            return $sortie->toArray();
+        }
+
+        if($data)
+            $sortie->setResultat(true);
+        else
+            $sortie->setErreur(new Exception("Echec de la requête"));
+    }
+    else
+    {
+        $sortie->setErreur(new Exception("Contact inconnu"));
+    }
+
     return $sortie->toArray();
 }
